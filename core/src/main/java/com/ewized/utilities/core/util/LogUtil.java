8a89373d477a1854c66c5b9e26e64e7efdae3b65
Joshua Rodriguez <ewized@gmail.com>
2014-07-17 16:31:50 -0700

    Allow setting the log level

--- core/src/main/java/com/ewized/utilities/core/util/LogUtil.java
+++ core/src/main/java/com/ewized/utilities/core/util/LogUtil.java
@@ -3,6 +3,7 @@ package com.ewized.utilities.core.util;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Data
@@ -10,25 +11,37 @@ import java.util.logging.Logger;
 @SuppressWarnings("unused")
 public class LogUtil {
     protected Logger logger;
+    protected Level level;
     protected boolean debug;
 
     public LogUtil() {
-        this(Logger.getLogger(LogUtil.class.getName()), Boolean.parseBoolean(System.getProperty("debug")));
+        this(Logger.getLogger(LogUtil.class.getName()));
     }
 
     public LogUtil(Logger logger) {
-        this(logger, false);
+        this(logger, Level.INFO, Boolean.valueOf(System.getProperty("debug")));
+    }
+
+    public LogUtil(Logger logger, boolean debug) {
+        this(logger, Level.INFO, debug);
+    }
+
+    public LogUtil(Logger logger, Level level) {
+        this(logger, level, Boolean.valueOf(System.getProperty("debug")));
     }
 
     /** Logs a message to the console */
     public void log(String message, Object... args) {
-        logger.info(String.format(MessageUtil.stripColors(message), args));
+        logger.log(level, String.format(MessageUtil.stripColors(message), args));
     }
 
     /** Logs a debug message to the console */
     public void debug(String message, Object... args) {
         if (debug) {
+            Level old = level;
+            setLevel(Level.WARNING);
             log("DEBUG: " + MessageUtil.stripColors(message), args);
+            setLevel(level);
         }
     }
 
