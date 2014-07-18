f340eceb82eba61eebc71664f915656c05505c7b
Joshua Rodriguez <ewized@gmail.com>
2014-07-18 16:51:01 -0700

    Parse JSON object from url for URLLocaleManager

--- core/src/main/java/com/ewized/utilities/core/util/locale/URLLocaleManager.java
+++ core/src/main/java/com/ewized/utilities/core/util/locale/URLLocaleManager.java
@@ -1,11 +1,17 @@
 package com.ewized.utilities.core.util.locale;
 
+import com.google.gson.Gson;
 import lombok.NoArgsConstructor;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 
 @NoArgsConstructor
+@SuppressWarnings("unused")
 public class URLLocaleManager extends AbstractLocaleManager {
+    private static final Gson gson = new Gson();
+
     /**
      * Load the locale files once created.
      * @param url The url object the use.
@@ -34,6 +40,19 @@ public class URLLocaleManager extends AbstractLocaleManager {
             } catch (Exception e) {
                 log.log(e, false);
             }
+        }
+    }
+
+    /**
+     * Creates an string array based on the provided url.
+     * @param url The url that is a json object
+     * @return String[] of locale codes, or default to DEFAULT_LOCALE
+     */
+    public static String[] parseJson(String url) {
+        try {
+            return gson.fromJson(new InputStreamReader(new URL(url).openStream()), String[].class);
+        } catch (IOException e) {
+            return new String[] {DEFAULT_LOCALE};
         }
     }
 }
