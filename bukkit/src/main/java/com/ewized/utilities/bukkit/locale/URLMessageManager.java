9160f87a3e7720402959e56d1e3afdd5b182cd19
Joshua Rodriguez <ewized@gmail.com>
2014-11-11 19:09:23 -0800

    Update locale urls

--- bukkit/src/main/java/com/ewized/utilities/bukkit/locale/URLMessageManager.java
+++ bukkit/src/main/java/com/ewized/utilities/bukkit/locale/URLMessageManager.java
@@ -6,7 +6,7 @@ import com.ewized.utilities.locale.URLLocaleManager;
 
 class URLMessageManager extends URLLocaleManager {
     private static QuickCache<URLMessageManager> inst = QuickCache.builder(URLMessageManager.class).build();
-    private static final String LOCALE_URL = "https://git.year4000.net/year4000/locales/raw/master/com/ewized/utilities/locales/";
+    private static final String LOCALE_URL = "https://raw.githubusercontent.com/Year4000/Locales/master/net/year4000/utilities/locales/";
 
     public URLMessageManager() {
         super(Utilities.getInst().getLog(), LOCALE_URL, parseJson(LOCALE_URL + LOCALES_JSON));
